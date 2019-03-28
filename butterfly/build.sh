895867460825b16e029fc7e298c7c31eec85a194
DStack Group <dstackgroup@gmail.com>
2019-03-28 22:23:40 +0100

    RedmineProducer: docker setup

--- butterfly/build.sh
+++ butterfly/build.sh
@@ -4,8 +4,12 @@ docker run --rm \
   -v /${PWD}:/butterfly \
   -v /${HOME}/.m2:/root/.m2 \
   -v /${PWD}/target:/butterfly/target \
-  -v /${PWD}/config/target:/butterfly/config/target \
+  -v /${PWD}/common/target:/butterfly/common/target \
+  -v /${PWD}/consumer/target:/butterfly/consumer/target \
+  -v /${PWD}/email-consumer/target:/butterfly/email-consumer/target \
   -v /${PWD}/gitlab-producer/target:/butterfly/gitlab-producer/target \
   -v /${PWD}/producer/target:/butterfly/producer/target \
+  -v /${PWD}/redmine-producer/target:/butterfly/redmine-producer/target \
+  -v /${PWD}/telegram-consumer/target:/butterfly/telegram-consumer/target \
   -w //butterfly \
   maven:3.6-jdk-11-slim mvn verify package
