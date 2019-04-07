915f0f90d671d6ceeef21f9ef23d379f28551ff7
Federico Rispo <dev@federicorispo.it>
2019-04-07 21:33:35 +0200

    Added unit tests for the Record class

---
+++ butterfly/controller/src/test/java/it/unipd/dstack/butterfly/controller/record/utils/UtilsTest.java
@@ -0,0 +1,47 @@
+package it.unipd.dstack.butterfly.middleware.dispatcher.utils;
+
+import it.unipd.dstack.butterfly.middleware.dispatcher.utils.Utils;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class RecordTest {
+
+    private Record<Integer> integerRecord;
+    private Record<String> stringRecord;
+
+    public RecordTest() {
+        integerRecord = new Record<Integer>("IntegerTest", 10);
+        stringRecord = new Record<String>("StringTest", "Ten");
+    }
+
+
+    /**
+     *  <p>Check if the getTopic method returns the Topic string of the Record<p>
+     *
+     *  @author DStack Group
+     *  @return void
+     */
+
+    @Test
+    public void shouldReturnTopicOfTheRecord() {
+        assertEquals("IntegerTest", integerRecord.getTopic());
+        assertEquals("StringTest", stringRecord.getTopic());
+    }
+
+
+    /**
+     *  <p>Check if the getData method returns the Data of the Record<p>
+     *
+     *  @author DStack Group
+     *  @return void
+     */
+
+    @Test
+    public void shouldReturnDataOfTheRecord() {
+        assertTrue(10 == integerRecord.getData());
+        assertEquals("Ten", stringRecord.getData());
+    }
+}
