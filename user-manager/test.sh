f81fd4f1ed371156cdb48fc5cdc691b4aa7bc803
DStack Group <dstackgroup@gmail.com>
2019-04-09 01:25:15 +0200

    user-manager: Docker's integration tests now install dependencies by themselves

--- user-manager/test.sh
+++ user-manager/test.sh
@@ -1,2 +1,4 @@
+docker-compose -f docker-compose.install.yml down -v;
+docker-compose -f docker-compose.install.yml up --build;
 docker-compose -f docker-compose.test.yml down -v;
 docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit;
