978c8b8b946746c4c2fe8ca84694d833a7700724
DStack Group <dstackgroup@gmail.com>
2019-04-11 17:00:06 +0200

    gitlab-producer: updated gitlab4j to version 4.9.22

--- butterfly/gitlab-producer/src/main/java/it/unipd/dstack/butterfly/producer/gitlab/webhookmanager/GitlabWebhookListenerAggregator.java
+++ butterfly/gitlab-producer/src/main/java/it/unipd/dstack/butterfly/producer/gitlab/webhookmanager/GitlabWebhookListenerAggregator.java
@@ -1,6 +1,9 @@
 package it.unipd.dstack.butterfly.producer.gitlab.webhookmanager;
 
+import it.unipd.dstack.butterfly.producer.gitlab.GitlabProducerController;
 import it.unipd.dstack.butterfly.producer.producer.OnWebhookEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * GitlabWebhookListenerAggregator performs the same single action for every different event
@@ -8,6 +11,7 @@ import it.unipd.dstack.butterfly.producer.producer.OnWebhookEvent;
  * @param <T>
  */
 public class GitlabWebhookListenerAggregator<T> implements GitlabWebhookListener<T> {
+    private static final Logger logger = LoggerFactory.getLogger(GitlabWebhookListenerAggregator.class);
     private final OnWebhookEvent<T> onWebhookEvent;
 
     public GitlabWebhookListenerAggregator(OnWebhookEvent<T> onWebhookEvent) {
@@ -16,16 +20,19 @@ public class GitlabWebhookListenerAggregator<T> implements GitlabWebhookListener
 
     @Override
     public void onIssueCreatedEvent(T event) {
+        logger.info("onIssueCreatedEvent called " + event);
         this.onWebhookEvent.handleEvent(event);
     }
 
     @Override
     public void onIssueEditedEvent(T event) {
+        logger.info("onIssueEditedEvent called " + event);
         this.onWebhookEvent.handleEvent(event);
     }
 
     @Override
     public void onCommitCreatedEvent(T event) {
+        logger.info("onCommitCreatedEvent called " + event);
         this.onWebhookEvent.handleEvent(event);
     }
 }
