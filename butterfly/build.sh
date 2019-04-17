0bf8212f6548fcdaf9548345669e225d99114179
DStack Group <dstackgroup@gmail.com>
2019-04-18 01:39:01 +0200

    Now Java compilation is faster. butterfly/build.sh now permits to compile a single module at once, and disables tests execution by default.

--- butterfly/build.sh
+++ butterfly/build.sh
@@ -1,15 +1,59 @@
 #/bin/bash
 
+# Tests are disabled by default in order to have faster build times.
+
+print_usage() {
+  echo "Usage:
+  $0 [MODULE] {-h|-t|-i|}
+  
+  Commands:
+    MODULE               If specified, only compiles that single Maven module.
+  
+  Options:
+    --help,-h            Displays this manual.
+    --test,-t            Executes tests. Disabled by default.
+    --install,-i         Cleans the artifacts and compiles the project from scratch.
+" >&2;
+}
+
+INSTALL_COMMAND="";
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -h|--help) print_usage; exit 0;;
+    -t|--test) SHOULD_TEST=1;;
+    -i|--install) INSTALL_COMMAND="install";;
+    -*) echo "Error: unknown option $1" >&2; print_usage; exit 1;;
+    *) MODULE_TO_BUILD="$1"; shift;;
+  esac;
+  shift;
+done
+
+if [ -z $MODULE_TO_BUILD ];
+then OPTION="-T5C"; ARGUMENT="$1";
+else OPTION="-pl $MODULE_TO_BUILD -T5C"; ARGUMENT="$2"; 
+fi
+
+if [ -z $SHOULD_TEST ];
+then OPTION+=" -DskipTests";
+fi
+
+COMMAND="mvn $INSTALL_COMMAND package $OPTION";
+echo "Executing command $COMMAND";
+
 docker run --rm \
   -v /${PWD}:/butterfly \
   -v /${HOME}/.m2:/root/.m2 \
-  -v /${PWD}/target:/butterfly/target \
+  -v /${PWD}/avro-schemas/target:/butterfly/avro-schemas/target \
   -v /${PWD}/common/target:/butterfly/common/target \
+  -v /${PWD}/config/target:/butterfly/config/target \
   -v /${PWD}/consumer/target:/butterfly/consumer/target \
+  -v /${PWD}/controller/target:/butterfly/controller/target \
   -v /${PWD}/email-consumer/target:/butterfly/email-consumer/target \
+  -v /${PWD}/event-processor/target:/butterfly/event-processor/target \
   -v /${PWD}/gitlab-producer/target:/butterfly/gitlab-producer/target \
   -v /${PWD}/producer/target:/butterfly/producer/target \
   -v /${PWD}/redmine-producer/target:/butterfly/redmine-producer/target \
   -v /${PWD}/telegram-consumer/target:/butterfly/telegram-consumer/target \
   -w //butterfly \
-  maven:3.6-jdk-11-slim mvn package -T5C
+  maven:3.6-jdk-11-slim $COMMAND;
--- butterfly/clean.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-docker run --rm \
-  -v /${PWD}:/butterfly \
-  -v /${HOME}/.m2:/root/.m2 \
-  -w //butterfly \
-  maven:3.6-jdk-11-slim mvn clean
