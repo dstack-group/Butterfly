a4049bccdb70e2757b64ed4258962b507b3980ca
DStack Group <dstackgroup@gmail.com>
2019-04-18 20:57:08 +0200

    Improved configuration files

--- butterfly/build.sh
+++ butterfly/build.sh
@@ -4,25 +4,28 @@
 
 print_usage() {
   echo "Usage:
-  $0 [MODULE] {-h|-t|-i|}
+  $0 [MODULE] {-h|-c|-t|-i|}
   
   Commands:
     MODULE               If specified, only compiles that single Maven module.
   
   Options:
     --help,-h            Displays this manual.
-    --test,-t            Executes tests. Disabled by default.
+    --clean,-c           Clears the target folders.
+    --test,-t            Executes tests. Disabled by default. When it is enabled,
+                         parallel compilation is prevented.
     --install,-i         Cleans the artifacts and compiles the project from scratch.
 " >&2;
 }
 
-INSTALL_COMMAND="";
+SUBCOMMAND="";
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     -h|--help) print_usage; exit 0;;
+    -c|--clean) SUBCOMMAND+="clean ";;
     -t|--test) SHOULD_TEST=1;;
-    -i|--install) INSTALL_COMMAND="install";;
+    -i|--install) SUBCOMMAND+="install ";;
     -*) echo "Error: unknown option $1" >&2; print_usage; exit 1;;
     *) MODULE_TO_BUILD="$1"; shift;;
   esac;
@@ -30,7 +33,11 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [ -z $MODULE_TO_BUILD ];
-then OPTION="-T5C"; ARGUMENT="$1";
+then
+  if [ -z $SHOULD_TEST ]
+  then OPTION="-T5C";
+  fi
+  ARGUMENT="$1";
 else OPTION="-pl $MODULE_TO_BUILD -T5C"; ARGUMENT="$2"; 
 fi
 
@@ -38,21 +45,11 @@ if [ -z $SHOULD_TEST ];
 then OPTION+=" -DskipTests";
 fi
 
-COMMAND="mvn $INSTALL_COMMAND package $OPTION";
+COMMAND="mvn ${SUBCOMMAND}package $OPTION";
 echo "Executing command $COMMAND";
 
 docker run --rm \
   -v /${PWD}:/butterfly \
   -v /${HOME}/.m2:/root/.m2 \
-  -v /${PWD}/avro-schemas/target:/butterfly/avro-schemas/target \
-  -v /${PWD}/config/target:/butterfly/config/target \
-  -v /${PWD}/consumer/target:/butterfly/consumer/target \
-  -v /${PWD}/controller/target:/butterfly/controller/target \
-  -v /${PWD}/email-consumer/target:/butterfly/email-consumer/target \
-  -v /${PWD}/event-processor/target:/butterfly/event-processor/target \
-  -v /${PWD}/gitlab-producer/target:/butterfly/gitlab-producer/target \
-  -v /${PWD}/producer/target:/butterfly/producer/target \
-  -v /${PWD}/redmine-producer/target:/butterfly/redmine-producer/target \
-  -v /${PWD}/telegram-consumer/target:/butterfly/telegram-consumer/target \
   -w //butterfly \
   maven:3.6-jdk-11-slim $COMMAND;
