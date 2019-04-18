a913b2d33a37cefa725fcb88cb0dc6a7a8a80b49
DStack Group <dstackgroup@gmail.com>
2019-04-18 11:59:16 +0200

    Fix Travis

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 set -e;
 echo "Building and testing Java modules";
-cd ./butterfly; ./build.sh --install --test; cd ..;
+cd ./butterfly; ./build.sh --test; cd ..;
 echo "Building and testing User Manager module";
 cd ./user-manager; ./test.sh; cd ..;
