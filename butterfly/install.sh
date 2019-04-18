c9d67798f9f12f611cebb861f57a3eeda6b6300e
DStack Group <dstackgroup@gmail.com>
2019-04-18 22:15:27 +0200

    Fixed path problem

--- butterfly/install.sh
+++ butterfly/install.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 docker run --rm \
-  -v /${PWD}:/butterfly \
-  -v /${HOME}/.m2:/root/.m2 \
+  -v "/${PWD}":/butterfly \
+  -v "/${HOME}"/.m2:/root/.m2 \
   -w //butterfly \
   maven:3.6-jdk-11-slim mvn clean test-compile dependency:resolve \
     -Dmaven.main.skip=true \
