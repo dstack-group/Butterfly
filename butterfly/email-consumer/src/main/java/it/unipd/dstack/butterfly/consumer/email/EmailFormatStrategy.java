56dc099467364ce0e4b1b98a854479fd1aa5537b
Enrico <enricotrinco95@hotmail.it>
2019-04-19 18:06:59 +0200

    fix error pre presentazione

--- butterfly/email-consumer/src/main/java/it/unipd/dstack/butterfly/consumer/email/EmailFormatStrategy.java
+++ butterfly/email-consumer/src/main/java/it/unipd/dstack/butterfly/consumer/email/EmailFormatStrategy.java
@@ -13,6 +13,12 @@ public class EmailFormatStrategy implements FormatStrategy<EventWithUserContact>
     @Override
     public String format(EventWithUserContact eventWithUserContact) {
         var event = eventWithUserContact.getEvent();
-        return String.format("%s<br/>%s", event.getTitle(), event.getDescription());
+
+        var intro = String.format("Hi %s %s, here's a new event for you.<br/><br/>",
+                eventWithUserContact.getUserContact().getFirstname(),
+                eventWithUserContact.getUserContact().getLastname());
+        var content = String.format("%s<br/>%s", event.getTitle(), event.getDescription());
+
+        return String.format("%s %s", intro, content);
     }
 }
