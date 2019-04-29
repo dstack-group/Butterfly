97596ef677f66cdc43e3b9091f21673f90009f62
Federico Rispo <dev@federicorispo.it>
2019-04-29 17:07:33 +0200

    [CLI] Fixed RestRequests and UserRestRequests
    RestRequests:
      - Added HttpMethod enumerator
      - Fixed linting errors
    
    UserRestRequests
      - Adapted submitRequest() call
      - Fixed linting errors

--- user-manager/bumcli/src/rest-client/RestRequests.ts
+++ user-manager/bumcli/src/rest-client/RestRequests.ts
@@ -3,6 +3,18 @@ import { RequestException} from './exception';
 
 import fetch from 'node-fetch';
 
+/*
+ * This enumerator represents every http method that can be
+ * used to make http request in the submitRequest()
+ */
+export enum HttpMethod {
+  GET = 'GET',
+  DELETE = 'DELETE',
+  PATCH = 'PATCH',
+  POST = 'POST',
+  PUT = 'PUT',
+}
+
 export abstract class RestRequests {
 
   private server: ServerConfig;
@@ -12,47 +24,45 @@ export abstract class RestRequests {
 
     // TODO: Fetch this variables from config file
     this.server = {
-      hostname: "http://localhost",
+      hostname: 'http://localhost',
       port: 5000,
-      timeout: 1000
-    }
+      timeout: 1000,
+    };
 
     this.headers = {'Content-Type': 'application/json'};
   }
 
   private getURL(): string {
-    return this.server.hostname + ":" + this.server.port;
+    return `${this.server.hostname}:${this.server.port}`;
   }
 
-
-  submitRequest<T>(endpoint: string, _method: string, _body?: object): Promise<T> {
+  submitRequest<T>(endpoint: string, method: HttpMethod, body?: object): Promise<T> {
 
     // Create options parameters necessary for the request
     const options = {
-      method: _method,
-      body: _body ? JSON.stringify(_body) : undefined,
-      headers:this.headers,
-      timeout: this.server.timeout
+      body: body ? JSON.stringify(body) : undefined,
+      headers: this.headers,
+      method: HttpMethod[method],
+      timeout: this.server.timeout,
     };
 
     return new Promise(async (resolve, reject) => {
       try {
-        const response = await fetch(this.getURL() + "/" + endpoint, options);
-        const body = await response.json();
+        const response = await fetch(`${this.getURL()}/${endpoint}`, options);
+        const content = await response.json();
 
         /*
          * The <code>ok</code> method checks if the code of the
          * request response  is >= 200 and <= 300
          */
-
-        if(response.ok)
-          resolve(body['data'] as T);
-        else
-          reject(new RequestException({code: body.code, message: body.message}));
-      }
-      catch(err) {
+        if (response.ok) {
+          resolve(content.data as T);
+        } else {
+          reject(new RequestException({code: content.code, message: content.message}));
+        }
+      } catch (error) {
         // Necessary to catch all the Network errors
-        reject(err);
+        reject(error);
       }
     });
   }
--- user-manager/bumcli/src/rest-client/UserRestRequests.ts
+++ user-manager/bumcli/src/rest-client/UserRestRequests.ts
@@ -1,26 +1,26 @@
 import { User, CreateUser, UpdateUser, FindUser, RemoveUser } from './entities/UserEntities';
-import { RestRequests } from './RestRequests';
+import { RestRequests, HttpMethod } from './RestRequests';
 
 
 export class UserRestRequests extends RestRequests {
 
   create(user: CreateUser): Promise<User> {
-    return this.submitRequest<User>("users", 'POST', user);
+    return this.submitRequest<User>('users', HttpMethod.POST, user);
   }
 
   update(user: UpdateUser): Promise<User> {
-    return this.submitRequest<User>("users/" + user.email, 'PATCH', user);
+    return this.submitRequest<User>(`users/${user.email}`, HttpMethod.PATCH, user);
   }
 
   find(user: FindUser): Promise<User> {
-    return this.submitRequest<User>("users/" + user.email, 'GET');
+    return this.submitRequest<User>(`users/${user.email}`, HttpMethod.GET);
   }
 
   findAll(): Promise<User[]> {
-    return this.submitRequest<User[]>("users", 'GET');
+    return this.submitRequest<User[]>('users', HttpMethod.GET);
   }
 
   remove(user: RemoveUser): void {
-    this.submitRequest<void>("users/" + user.email, 'DELETE');
+    this.submitRequest<void>(`users/${user.email}`, HttpMethod.DELETE);
   }
 }
