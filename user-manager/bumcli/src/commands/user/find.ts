e6da28a1748aa96e7218fe8fcf0902435e869f84
Federico Rispo <dev@federicorispo.it>
2019-04-30 07:15:54 +0200

    [CLI] Updated find command to test the LocalDb

--- user-manager/bumcli/src/commands/user/find.ts
+++ user-manager/bumcli/src/commands/user/find.ts
@@ -3,10 +3,16 @@ import { RestRequests } from '../../rest-client/RestRequests'
 import {UserRestRequests} from '../../rest-client';
 import { BaseCommand } from '../../base/base'
 import { User } from '../../rest-client/entities/UserEntities'
+import { LocalDb, Config } from '../../database/LocalDb';
+import { ServerConfig } from '../../rest-client/ServerConfig';
 
-
+/*
+ * THIS IS A TESTING COMMAND: Does not respect any type of conventions
+ */
 export default class Find extends BaseCommand {
 
+  // db must be moved into BaseCommand. Its here only for quick tests
+  private db: LocalDb = new LocalDb('db.json');
   private client: UserRestRequests = new UserRestRequests();
 
   static description = 'Find a user by email';
@@ -27,13 +33,19 @@ export default class Find extends BaseCommand {
     /*if(cmdParams.flags.email.length > 0) {
       console.log(await this.client.find({email: cmdParams.flags.email}));
     }*/
-
+/*
     const datiUser: User = await this.client.find({email: cmdParams.flags.email});
     if(cmdParams.flags.table){
       this.list(datiUser)
     } else {
       console.log(datiUser)
     }
+    */
+    const sc: ServerConfig = {hostname: 'http://localhost', port: 5000, timeout: 1000};
+
+    this.db.setValues(Config.Server, sc);
+    console.log(this.db.getValues(Config.Server));
+    
 
   }
 }
