c7d073a4e7cd1609ea88906820f3b9f963e0148d
Federico Rispo <dev@federicorispo.it>
2019-05-01 08:01:16 +0200

    [CLI] Updated user:find command (testing)

--- user-manager/bumcli/src/commands/user/find.ts
+++ user-manager/bumcli/src/commands/user/find.ts
@@ -1,39 +1,34 @@
-import {Command, flags} from '@oclif/command';
-import { RestRequests } from '../../rest-client/RestRequests'
-import {UserRestRequests} from '../../rest-client';
+import { flags } from '@oclif/command';
+import { UserRestRequests } from '../../rest-client';
 import { BaseCommand } from '../../base/base'
 import { User } from '../../rest-client/entities/UserEntities'
-import { LocalDb, Config } from '../../database/LocalDb';
-import { ServerConfig } from '../../rest-client/ServerConfig';
+import { Config } from '../../database/LocalDb';
+import { Validator } from '../../utils/Validator';
 
 /*
  * THIS IS A TESTING COMMAND: Does not respect any type of conventions
  */
 export default class Find extends BaseCommand {
 
-  // db must be moved into BaseCommand. Its here only for quick tests
-  private db: LocalDb = new LocalDb('db.json');
-  private client: UserRestRequests = new UserRestRequests();
+  private client: UserRestRequests = new UserRestRequests(this.db.getValues(Config.Server));
 
   static description = 'Find a user by email';
 
   static flags = {
+    email: flags.string({char: 'e', description: 'email address [required]', required: true}),
     help: flags.help({char: 'h'}),
-    email: flags.string({char: 'e', description: 'email address [required]'}),
     table: flags.boolean({char: 't', description: 'yada yada'})
   };
-  
-
 
   async run() {
 
-    const cmdParams = this.parse(Find);
+    const flagss = this.parse(Find).flags;
 
-    //Some checking (this is only an example)
-    /*if(cmdParams.flags.email.length > 0) {
-      console.log(await this.client.find({email: cmdParams.flags.email}));
-    }*/
-/*
+    if (Validator.isEmailValid(flagss.email)) {
+      console.log(await this.client.find({email: flagss.email}));
+    }
+
+    /*
     const datiUser: User = await this.client.find({email: cmdParams.flags.email});
     if(cmdParams.flags.table){
       this.list(datiUser)
@@ -41,11 +36,5 @@ export default class Find extends BaseCommand {
       console.log(datiUser)
     }
     */
-    const sc: ServerConfig = {hostname: 'http://localhost', port: 5000, timeout: 1000};
-
-    this.db.setValues(Config.Server, sc);
-    console.log(this.db.getValues(Config.Server));
-    
-
   }
 }
