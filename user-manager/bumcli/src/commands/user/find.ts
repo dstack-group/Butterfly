3a3034f70826dee5a4ef270a81c01b39cbf7292e
Federico Rispo <dev@federicorispo.it>
2019-05-01 09:08:37 +0200

    [CLI] Updated user:find command

--- user-manager/bumcli/src/commands/user/find.ts
+++ user-manager/bumcli/src/commands/user/find.ts
@@ -1,40 +1,36 @@
 import { flags } from '@oclif/command';
 import { UserRestRequests } from '../../rest-client';
-import { BaseCommand } from '../../base/base'
-import { User } from '../../rest-client/entities/UserEntities'
+import { BaseCommand } from '../../base/base';
+import { User } from '../../rest-client/entities/UserEntities';
 import { Config } from '../../database/LocalDb';
 import { Validator } from '../../utils/Validator';
 
-/*
- * THIS IS A TESTING COMMAND: Does not respect any type of conventions
- */
 export default class Find extends BaseCommand {
-
-  private client: UserRestRequests = new UserRestRequests(this.db.getValues(Config.Server));
 
   static description = 'Find a user by email';
 
   static flags = {
     email: flags.string({char: 'e', description: 'email address [required]', required: true}),
     help: flags.help({char: 'h'}),
-    table: flags.boolean({char: 't', description: 'yada yada'})
+    table: flags.boolean({char: 't', description: 'display results in table form'}),
   };
 
   async run() {
+    try {
+      const client: UserRestRequests = new UserRestRequests(this.db.getValues(Config.Server));
+      const flagss = this.parse(Find).flags;
 
-    const flagss = this.parse(Find).flags;
+      if (Validator.isEmailValid(flagss.email)) {
+        const dataUser: User = await client.find({email: flagss.email});
 
-    if (Validator.isEmailValid(flagss.email)) {
-      console.log(await this.client.find({email: flagss.email}));
+        if (flagss.table) {
+          this.lista(dataUser);
+        } else {
+          this.log(dataUser);
+        }
+      }
+    } catch (error) {
+      this.error(error.message);
     }
-
-    /*
-    const datiUser: User = await this.client.find({email: cmdParams.flags.email});
-    if(cmdParams.flags.table){
-      this.list(datiUser)
-    } else {
-      console.log(datiUser)
-    }
-    */
   }
 }
