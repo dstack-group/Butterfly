a892e127ca9a4d766b2d0fef6f583301d52b79b5
DStack Group <dstackgroup@gmail.com>
2019-05-01 16:34:30 +0200

    Removed unused validateRequest export

--- user-manager/user-manager-rest-api/src/middlewares/index.ts
+++ user-manager/user-manager-rest-api/src/middlewares/index.ts
@@ -16,6 +16,5 @@
 export { bodyParser } from './bodyParser';
 export { compress } from './compress';
 export { errorHandler } from './errorHandler';
-export { validateRequest } from './validateRequest';
 export { logRouteRequest } from './logRouteRequest';
 export { headers } from './headers';
