ec701042993c68cb9b0db8b00ee61c050d0d12e7
Federico Rispo <dev@federicorispo.it>
2019-05-03 11:52:51 +0200

    [CLI] Improved Validator: now thrown CommandFlagException when the validation failed

--- user-manager/bumcli/src/utils/Validator.ts
+++ user-manager/bumcli/src/utils/Validator.ts
@@ -1,32 +1,44 @@
+import { CommandFlagException } from './../rest-client/exception/CommandFlagException';
+
 export class Validator {
 
-    // Every single parameter is already check that is not undefined
+  // Every single parameter is already check that is not undefined
 
-    static isStringValid(input: string, minLength: number, maxLength: number): boolean {
+  static isStringValid(flag: string, input: string, minLength: number, maxLength: number): string {
 
-        return input.length >= minLength && input.length <= maxLength;
+    if (input.length < minLength || input.length > maxLength) {
+      throw new CommandFlagException({
+        message: `Value must be between ${minLength} and ${maxLength} characters`,
+        nameFlag: flag,
+      });
     }
 
-    static isEmailValid(email: string): boolean {
-        const emailRegExp: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return input;
+  }
 
-        if (email.match(emailRegExp) === null) {
-            return false;
-        }
+  static isEmailValid(email: string): string {
+    const emailRegExp: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        return true;
+    if (email.match(emailRegExp) === null) {
+      throw new CommandFlagException({
+        message: 'The email address is not a valid format',
+        nameFlag: 'email',
+      });
     }
 
-    static isHostnameValid(hostname: string): boolean {
-        // TODO: Find a regexp for the hostname check
-        /*
-        const hostRegExp: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return email;
+  }
 
-        if (hostname.match(hostRegExp) === null) {
-            return false;
-        }
-        */
+  static isHostnameValid(hostname: string): string {
+    // TODO: Find a regexp for the hostname check
+    /*
+    const hostRegExp: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        return true;
+    if (hostname.match(hostRegExp) === null) {
+        return false;
     }
+    */
+
+    return hostname;
+  }
 }
