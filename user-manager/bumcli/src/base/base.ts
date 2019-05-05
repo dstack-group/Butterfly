d81fb7d8b22d4e9fc0327a091be5be716a784e7c
unknown <enricotrinco95@hotmail.it>
2019-05-05 12:14:43 +0200

    Adapted the table with cli.table on 'base' and implemented it on command 'find'

--- user-manager/bumcli/src/base/base.ts
+++ user-manager/bumcli/src/base/base.ts
@@ -1,7 +1,8 @@
 import { Command, flags } from '@oclif/command';
-import Table = require('cli-table');
-import chalk from 'chalk';
+import { Table, cli } from 'cli-ux'
 import { LocalDb } from '../database/LocalDb';
+
+export type TableColumns<T extends {}> = Table.table.Columns<T>;
 
 export abstract class BaseCommand extends Command {
 
@@ -18,73 +19,42 @@ export abstract class BaseCommand extends Command {
     this.db = new LocalDb(`db.json`);
   }
 
+  /*
   protected print(data: object | object, jsonEnabled: boolean): void {
     (jsonEnabled) ?
       this.log(this.showJSONFormat(data)) :
       this.log('table');
+  }*/
+
+
+  //Update: Moved here the Error condition because here do the check if 
+  protected print<T>(data: T[]): void {
+    (data.length === 0) ?
+      this.log(`Ops! Empty result`) :
+       this.log(this.showJSONFormat(data))
   }
 
-  private showJSONFormat(data: object): string {
-    return (Object.keys(data).length === 0) ?
-      `Ops! Empty result` :
+  //Update: insereted the condition if the array is a "single response" like create and find 
+  private showJSONFormat<T>(data: T[]): string {
+    return (data.length === 1) ?
+     JSON.stringify(data[0], null, 2) :
       JSON.stringify(data, null, 2);
   }
 
-  // The function show the result inside an ASCII table, giving to the user a better view of the result
-  protected lista(arrData: object[]): void {
-    
-    /*extracting the first element to find witch keys need to insert
-     * into the table's header
-     */
-    const data : object = arrData[0]
-    const dataKeys : string[] = Object.keys(data)
-    const numberOfJSONElement = dataKeys.length 
-    const arrDataJson = <JSON[]> arrData
-    const h: string[] = []
-   
-    for(let headValue = 0; headValue < numberOfJSONElement; ++headValue){
-    h.push(chalk.blueBright(dataKeys[headValue]))
-    }
 
-    //filling the header of the table information of the JSON
-    const table= new Table({head:h})
+  // New function: print che table with the passed result and header
+  protected printTable<T extends {}>(result: T[], columns: TableColumns<T>){
+    return cli.table(result,columns,{})
+  }
 
-    //Users specific info table
-    if(dataKeys[0] == "email"){
-      for (let i = 0; i < arrDataJson.length; i++) {
-        const dataJson = <JSON> arrDataJson[i];
-        const dataString = JSON.stringify(dataJson)
-        const dataToJson = JSON.parse(dataString)
-        if(Object.keys(dataToJson)[0] == dataKeys[0]){                                                        //da rivedere (Ha senso?)
-          table.push([dataToJson.email, dataToJson.firstname, dataToJson.lastname, dataToJson.enabled])
-        } else{
-          console.error("The JSON file is corrupted and can't show the table")
-          return;
-        }
-     }
-   } 
-
-    //Projects specific info table
-    else if (dataKeys[0] == "projectId"){
-      for (let i = 0; i < arrDataJson.length; ++i){
-        const dataJson = <JSON> arrDataJson[i];
-        const dataString = JSON.stringify(dataJson)
-        const dataToJson = JSON.parse(dataString)
-
-        //Copy of the inner JSON
-        const projectURLs = dataToJson.projectURL     
-    
-        //Check if the project have or not any URL inserted
-        if(!Object.keys(projectURLs)){
-            table.push([dataToJson.projectId, dataToJson.projectName, "Non sono presenti URL"])
-        } else {
-          projectURLs.REDMINE &&  table.push([dataToJson.projectId, dataToJson.projectName, projectURLs.REDMINE]) 
-          projectURLs.GITLAB &&  table.push([dataToJson.projectId, dataToJson.projectName, projectURLs.GITLAB])
-          projectURLs.SONARQUBE &&  table.push([dataToJson.projectId, dataToJson.projectName, projectURLs.SONARQUBE]) 
-        }
-      }
-    }
-    //table output
-    console.log(table.toString())
+  /* New function, check if the flag jsonEnabled are True or False.
+   * True  => call print that show the raw JSON
+   * False => call the printTable that show the result inside a table  
+  */
+  protected showResult<T extends{}>(result: T[], columns: TableColumns<T>, jsonEnabled: boolean): void
+  {
+    (jsonEnabled) ? 
+    this.print(result) : 
+    this.printTable(result,columns)
   }
 }
--- user-manager/bumcli/src/commands/user/find.ts
+++ user-manager/bumcli/src/commands/user/find.ts
@@ -1,8 +1,10 @@
 import { flags } from '@oclif/command';
 import { UserRestRequests } from '../../rest-client';
-import { BaseCommand } from '../../base/base';
+import { BaseCommand, TableColumns } from '../../base/base';
 import { Config } from '../../database/LocalDb';
 import { Validator } from '../../utils/Validator';
+//new
+import { FindUser, User } from '../../rest-client/entities/UserEntities';
 
 export class Find extends BaseCommand {
 
@@ -13,16 +15,41 @@ export class Find extends BaseCommand {
     email: flags.string({char: 'e', description: 'user email address'}),
   };
 
+
+  //New field: header for the table.
+  private static readonly UserColumns : TableColumns<User> = {
+    userId:{
+      minWidth : 7  
+    },
+    email :{
+      minWidth : 7
+    },
+    firstname :{
+      minWidth : 7
+    },
+    lastname : {
+      minWidth : 7
+    },
+    enabled :{
+      get: user => user.enabled ? 'T' : 'F'
+    },
+    created :{
+      minWidth : 7
+    },
+    modified :{
+      minWidth : 7
+    }
+  }
+
   async run() {
     try {
       const client: UserRestRequests = new UserRestRequests(this.db.getValues(Config.Server));
       const flagss = this.parse(Find).flags;
-
       if (flagss.email === undefined) {
-        this.print(await client.findAll(), flagss.json);
+        this.showResult(await client.findAll(), Find.UserColumns, flagss.json);
       } else {
         Validator.isEmailValid(flagss.email);
-        this.print(await client.find({email: flagss.email}), flagss.json);
+        this.showResult([await client.find({email: flagss.email})], Find.UserColumns, flagss.json);
       }
 
     } catch (error) {
