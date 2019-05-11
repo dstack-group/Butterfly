fc4dd740d35dfd8a3d40ab347cbaa4ce4ba13525
Federico Rispo <dev@federicorispo.it>
2019-05-11 11:21:02 +0200

    [CLI] Updated config:user and config:server command
     - Added table header
     - Added catch error

--- user-manager/bumcli/src/commands/config/server.ts
+++ user-manager/bumcli/src/commands/config/server.ts
@@ -1,5 +1,5 @@
 import { flags } from '@oclif/command';
-import { BaseCommand } from '../../base/base';
+import { BaseCommand, TableColumns } from '../../base/base';
 import { Validator } from '../../utils/Validator';
 import { Config } from '../../database/LocalDb';
 import { ServerConfig } from '../../database/ServerConfig';
@@ -25,32 +25,44 @@ export class Server extends BaseCommand {
       required: true,
     }),
 
-    timeout: flags.integer({char: 't', description: 'Set a timeout for the server connection'}),
+    timeout: flags.integer({ char: 't', description: 'Set a timeout for the server connection' }),
+  };
+
+  private static readonly columns: TableColumns<ServerConfig> = {
+    hostname: { minWidth: 30 },
+    port: { minWidth: 15 },
+    timeout: { minWidth: 15 },
   };
 
   async run() {
+    try {
 
-    const flagss = this.parse(Server).flags;
+      const flagss = this.parse(Server).flags;
 
-    // TODO: When all the values of ServerConfig are already memorized into the db
-    //       and the user set again only a subset of them I need to delete the
-    //       values not setted again? Now it removes them
-    const serverConfig: ServerConfig = {hostname: '', port: 0};
+      // TODO: When all the values of ServerConfig are already memorized into the db
+      //       and the user set again only a subset of them I need to delete the
+      //       values not setted again? Now it removes them
+      const serverConfig: ServerConfig = { hostname: '', port: 0 };
 
-    // Check if each flag exists before using it
+      // Check if each flag exists before using it
 
-    // Thanks to 'required' and 'dependOn'properties this check is not necessary
-    // (flagss.hostname !== undefined && flagss.port !== undefined)
+      // Thanks to 'required' and 'dependOn'properties this check is not necessary
+      // (flagss.hostname !== undefined && flagss.port !== undefined)
 
-    Validator.isURLValid(flagss.hostname);
-    serverConfig.hostname = flagss.hostname;
-    serverConfig.port = flagss.port;
+      Validator.isURLValid(flagss.hostname);
+      serverConfig.hostname = flagss.hostname;
+      serverConfig.port = flagss.port;
 
-    if (flagss.timeout !== undefined) {
-      serverConfig.timeout = flagss.timeout;
+      if (flagss.timeout !== undefined) {
+        serverConfig.timeout = flagss.timeout;
+      }
+
+      this.db.setValues(Config.Server, serverConfig);
+      const result = this.db.getValues(Config.Server);
+      this.showResult<ServerConfig>([result], Server.columns, flagss.json);
+
+    } catch (error) {
+      this.showError(error);
     }
-
-    this.db.setValues(Config.Server, serverConfig);
-    this.print(this.db.getValues(Config.Server), flagss.json);
   }
 }
--- user-manager/bumcli/src/commands/config/user.ts
+++ user-manager/bumcli/src/commands/config/user.ts
@@ -1,5 +1,5 @@
 import { flags } from '@oclif/command';
-import { BaseCommand } from '../../base/base';
+import { BaseCommand, TableColumns } from '../../base/base';
 import { Validator } from '../../utils/Validator';
 import { Config } from '../../database/LocalDb';
 import { UserInfo } from '../../database/UserInfo';
@@ -11,39 +11,48 @@ export class User extends BaseCommand {
 
   static flags = {
     ...BaseCommand.flags,
-    email: flags.string({char: 'e', description: 'Set a default email'}),
-    firstname: flags.string({char: 'f', description: 'Set your firstname'}),
-    lastname: flags.string({char: 'l', description: 'Set your lastname'}),
+    email: flags.string({ char: 'e', description: 'Set a default email' }),
+    firstname: flags.string({ char: 'f', description: 'Set your firstname' }),
+    lastname: flags.string({ char: 'l', description: 'Set your lastname' }),
+  };
+
+  private static readonly columns: TableColumns<UserInfo> = {
+    email: { minWidth: 40 },
+    firstname: { minWidth: 15 },
+    lastname: { minWidth: 15 },
   };
 
   async run() {
 
-    const flagss = this.parse(User).flags;
+    try {
+      const flagss = this.parse(User).flags;
 
-    // TODO: When all the values of UserInfo are already memorized into the db
-    //       and the user set again only a subset of them I need to delete the
-    //       values not setted again? Now it removes them
-    const userInfo: UserInfo = {};
+      // TODO: When all the values of UserInfo are already memorized into the db
+      //       and the user set again only a subset of them I need to delete the
+      //       values not setted again? Now it removes them
+      const userInfo: UserInfo = {};
 
-    // Check if each flag exists before using it
+      // Check if each flag exists before using it
 
-    if (flagss.email !== undefined) {
-      if (Validator.isEmailValid(flagss.email)) {
+      if (flagss.email !== undefined) {
+        Validator.isEmailValid(flagss.email);
         userInfo.email = flagss.email;
-      } else {
-        this.error(`${flagss.email} is an invalid email format!`);
       }
-    }
 
-    if (flagss.firstname !== undefined) {
-      userInfo.firstname = flagss.firstname;
-    }
+      if (flagss.firstname !== undefined) {
+        userInfo.firstname = flagss.firstname;
+      }
 
-    if (flagss.lastname !== undefined) {
-      userInfo.lastname = flagss.lastname;
-    }
+      if (flagss.lastname !== undefined) {
+        userInfo.lastname = flagss.lastname;
+      }
 
-    this.db.setValues(Config.User, userInfo);
-    this.print(this.db.getValues(Config.User), flagss.json);
+      this.db.setValues(Config.User, userInfo);
+      const result = this.db.getValues(Config.User);
+      this.showResult<UserInfo>([result], User.columns, flagss.json);
+
+    } catch (error) {
+      this.showError(error);
+    }
   }
 }
