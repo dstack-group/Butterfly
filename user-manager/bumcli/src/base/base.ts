24a30aa2f0836bd5f61d216fe508797720400d7b
Federico Rispo <dev@federicorispo.it>
2019-05-11 11:14:38 +0200

    [CLI] Update BaseCommand with new features
     - Improved table output
     - Added database checking during the creation of the command (init())
     - Added *--extended* flag to show all the columns of a table
     - Fixed some linting errors

--- user-manager/bumcli/src/base/base.ts
+++ user-manager/bumcli/src/base/base.ts
@@ -1,8 +1,11 @@
 import { Command, flags } from '@oclif/command';
-import { Table, cli } from 'cli-ux'
-import { LocalDb } from '../database/LocalDb';
+import { Table, cli } from 'cli-ux';
+import { LocalDb, Config } from '../database/LocalDb';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export type TableColumns<T extends {}> = Table.table.Columns<T>;
+const { extended } = cli.table.flags();
 
 export abstract class BaseCommand extends Command {
 
@@ -10,51 +13,66 @@ export abstract class BaseCommand extends Command {
   protected db: LocalDb;
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    json: flags.boolean({char: 'j', description: 'display results in json format'}),
-  }
+    extended,
+    help: flags.help({ char: 'h' }),
+    json: flags.boolean({ char: 'j', description: 'display results in json format' }),
+  };
 
   async init() {
-    // Path must be changed (${this.config.configDir}): its temporary and only for test
-    this.db = new LocalDb(`db.json`);
+
+    const dbPath: string = path.join(this.config.configDir, 'db.json');
+
+    // Check if the db exists
+    fs.access(dbPath, fs.constants.F_OK, error => {
+      if (error) {
+        error.message = `Local db not found at ${dbPath}\n `;
+        error.message += `Make sure that the directory exist!\n `;
+        error.message += `Use "buttercli init" command to create the db`;
+        throw error;
+      }
+    });
+
+    // Open db
+    this.db = new LocalDb(dbPath);
+
+    // Check if the db contains the right json structure
+    if (JSON.stringify(this.db.getValues(Config.Server)) === '{}') {
+      let message = 'The server connection is not correctly configured!\n ';
+      message += 'Use "buttercli init" command to initialize the db';
+      this.error(message);
+    }
   }
 
-  /*
-  protected print(data: object | object, jsonEnabled: boolean): void {
-    (jsonEnabled) ?
-      this.log(this.showJSONFormat(data)) :
-      this.log('table');
-  }*/
-
-
-  //Update: Moved here the Error condition because here do the check if 
-  protected print<T>(data: T[]): void {
+  private print<T>(data: T[]): void {
     (data.length === 0) ?
-      this.log(`Ops! Empty result`) :
-       this.log(this.showJSONFormat(data))
+      this.log('Ops! Empty result') :
+      this.log(this.showJSONFormat(data));
   }
 
-  //Update: insereted the condition if the array is a "single response" like create and find 
+  // Test if the array is a "single response" like create and find
   private showJSONFormat<T>(data: T[]): string {
     return (data.length === 1) ?
-     JSON.stringify(data[0], null, 2) :
+      JSON.stringify(data[0], null, 2) :
       JSON.stringify(data, null, 2);
   }
 
-
   // New function: print che table with the passed result and header
-  protected printTable<T extends {}>(result: T[], columns: TableColumns<T>){
-    return cli.table(result,columns,{})
+  private printTable<T extends {}>(result: T[], columns: TableColumns<T>) {
+    return cli.table(result, columns, {});
   }
 
-  /* New function, check if the flag jsonEnabled are True or False.
+  /**
+   * New function, check if the flag jsonEnabled are True or False
    * True  => call print that show the raw JSON
-   * False => call the printTable that show the result inside a table  
-  */
-  protected showResult<T extends{}>(result: T[], columns: TableColumns<T>, jsonEnabled: boolean): void
-  {
-    (jsonEnabled) ? 
-    this.print(result) : 
-    this.printTable(result,columns)
+   * False => call the printTable that show the result inside a table
+   */
+  protected showResult<T extends {}>(result: T[], columns: TableColumns<T>, jsonEnabled: boolean): void {
+    (jsonEnabled) ?
+      this.print(result) :
+      this.printTable(result, columns);
+  }
+
+  protected showError(error: Error) {
+    this.error(`Operation failed:\n ${error.message}`);
   }
 }
