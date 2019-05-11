a2b6273264f2ef01ae71192d8ba11b653d80e102
Federico Rispo <dev@federicorispo.it>
2019-05-11 11:11:43 +0200

    [CLI] Updated message content of the custom exceptions

--- user-manager/bumcli/src/exceptions/CommandFlagException.ts
+++ user-manager/bumcli/src/exceptions/CommandFlagException.ts
@@ -6,7 +6,7 @@ export interface FlagError {
 export class CommandFlagException extends Error {
 
   constructor(flagError: FlagError) {
-    super(`Invalid flag: ${flagError.nameFlag} | Message: ${flagError.message}`);
+    super(`Invalid flag: ${flagError.nameFlag}\n Message: ${flagError.message}`);
     this.name = 'Flag error';
   }
 }
--- user-manager/bumcli/src/rest-client/exception/RequestException.ts
+++ user-manager/bumcli/src/rest-client/exception/RequestException.ts
@@ -3,7 +3,7 @@ import { RequestError } from './RequestError';
 export class RequestException extends Error {
 
   constructor(error: RequestError) {
-    super(`Error code: ${error.code} | Message: ${error.message}`);
+    super(`Error code: ${error.code}\n Message: ${error.message}`);
     this.name = 'RequestException';
   }
 }
